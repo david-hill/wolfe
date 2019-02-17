81d32c24259c0f5e9456ee95dd32fb37a6469c3d
root <root@wolfe.orion>
2019-02-17 16:09:54 -0600

    Fixed rsync command.

--- configure.sh
+++ configure.sh
@@ -4,3 +4,5 @@ mkdir -p /root/.ssh
 cp root/.ssh/* /root/.ssh/
 chmod 600 /root/.ssh/authorized_keys
 
+#rsync --progress --inplace -a -v -e ssh  root@192.168.1.3:/var/lib/jenkins/ /var/lib/jenkins/ --exclude VMs --exclude *.tar --exclude *.iso --exclude *.qcow2
+rsync --progress --inplace -a -v -e ssh  root@192.168.1.3:/var/lib/jenkins/ /var/lib/jenkins/ 
