76a43185b00aea7afccb42f9305229065a01ebd4
root <root@wolfe.orion>
2019-05-21 08:44:29 -0400

    Install dnf-utils instead of yum-utils.

--- configure.sh
+++ configure.sh
@@ -22,7 +22,7 @@ function enable_start {
   fi
 }
 
-yum -y install vim jenkins python2-libvirt gcc redhat-rpm-config python2-devel tuned lm_sensors ntp libvirt docker ncurses-compat-libs net-snmp-utils net-snmp numactl ntpdate uptimed tmux screen dbus-tools terminator ncurses-devel flex bison openssl-devel elfutils-libelf-devel yum-utils
+yum -y install vim jenkins python2-libvirt gcc redhat-rpm-config python2-devel tuned lm_sensors ntp libvirt docker ncurses-compat-libs net-snmp-utils net-snmp numactl ntpdate uptimed tmux screen dbus-tools terminator ncurses-devel flex bison openssl-devel elfutils-libelf-devel dnf-utils
 
 pip list | grep virtualbmc
 if [ $? -ne 0 ]; then
